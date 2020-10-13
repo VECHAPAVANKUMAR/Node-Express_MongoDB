73c5d3849b9fca7064eb93d2583c4b86111b2618
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-10-13 21:32:13 +0530

    implemented REST API for dish comments endpoint

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -14,7 +14,7 @@ dishRouter.use(bodyParser.json());
 // All the below methods ALL, GET, POST, PUT, DELETE are grouped and are implemented
 // on the dishRouter and for this particular router all the methods are chained together
 dishRouter.route('/')
-
+// get all the dishes
 .get((req, res, next) => {
     Dishes.find({})
     .then((dishes) => {
@@ -25,6 +25,7 @@ dishRouter.route('/')
     }, err => next(err))
     .catch((err) => next(err))
 })
+// Post a Single Dish
 .post((req, res, next) => {
     // Since we applied the body parser whatever there in the body of the request and adds
     //  back to the req object as req.body we can directly create the dish by passing req.body
@@ -43,6 +44,7 @@ dishRouter.route('/')
     res.statusCode = 403;
     res.end(`PUT operation is not supported on /dishes`);
 })
+// delete all the dishes
 .delete((req, res, next) => {
     Dishes.deleteMany({})
     .then((resp) => {
@@ -54,6 +56,7 @@ dishRouter.route('/')
 })
 
 dishRouter.route('/:dishId')
+// get particular dish using dishId if exists
 .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
@@ -67,6 +70,7 @@ dishRouter.route('/:dishId')
     res.statusCode = 403;
     res.end(`POST operation not supported on /dishes/:${req.params.dishId}`)
 })
+// Update a paricular dish using dishId if exists
 .put((req, res, next) => {
     // To return the updated dish we need to enable the bew flag
     Dishes.findByIdAndUpdate(req.params.dishId, {
@@ -80,6 +84,7 @@ dishRouter.route('/:dishId')
     }, err => next(err))
     .catch((err) => next(err))
 })
+// delete that particular dish using dishId if exists
 .delete((req, res, next) => {
     Dishes.findByIdAndDelete(req.params.dishId)
     // Here dish is removed dish
@@ -87,6 +92,166 @@ dishRouter.route('/:dishId')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(dish);
+    }, err => next(err))
+    .catch((err) => next(err))
+})
+
+// For Dish Comments
+dishRouter.route('/:dishId/comments')
+// get the comments of the specified dish if exits
+.get((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        // if dish exists
+        if (dish !== null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            // res.json() will take json object as parameter and sents backs to the client
+            res.json(dish.comments);
+        } else {
+            err = new Error(`Dish ${req.params.dishId} not found`);
+            err.status = 404;
+            next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err))
+})
+// post a commengt to tha specified dish 
+.post((req, res, next) => {
+    // Since we applied the body parser whatever there in the body of the request and adds
+    //  back to the req object as req.body we can directly create the dish by passing req.body
+    // as parameter to Dishes.create()
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        // if dish exists
+        if (dish !== null) {
+            dish.comments.push(req.body);
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                // res.json() will take json object as parameter and sents backs to the client
+                res.json(dish);
+            })
+        } else {
+            err = new Error(`Dish ${req.params.dishId} not found`);
+            err.status = 404;
+            next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err))
+})
+// PUT operation on /dishes endpoint does not make any value 
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`PUT operation is not supported on /dishes/${req.params.dishId}/comments`);
+})
+// delethe all the comments that are existed for the specified dish
+.delete((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (dish !== null) {
+            // Here comments is the sub-document of dish we can not remove the commets
+            // directly. So, we need to iterate and delete one by one
+            for (let index = dish.comments.length - 1; index >= 0; index--) {
+                dish.comments.id(dish.comments[index]._id).remove();
+            }
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                // res.json() will take json object as parameter and sents backs to the client
+                res.json(dish);
+            })
+        } else {
+            err = new Error(`Dish ${req.params.dishId} not found`);
+            err.status = 404;
+            next(err);
+        }
+    })
+})
+
+dishRouter.route('/:dishId/comments/:commentId')
+// get the specified comment of specified dish if exists
+.get((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        // if dish exists and that particular comment exists in that dish comments
+        if (dish !== null && dish.comments.id(req.params.commentId) !== null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            // res.json() will take json object as parameter and sents backs to the client
+            res.json(dish.comments.id(req.params.commentId));
+        } else if (dish == null) {
+                err = new Error(`Dish ${req.params.dishId} not found`);
+                err.status = 404;
+                next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err))    .catch((err) => next(err))
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /dishes/:${req.params.dishId}/comments/${req.params.commentId}`)
+})
+// While updating the comment we need to allow the author name to be updated
+// we should update rating, comment only
+.put((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    // Here dish is removed dish
+    .then((dish) => {
+        if (dish !== null && dish.comments.id(req.params.commentId) !== null) {
+            if (req.body.rating) {
+                dish.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.comment) {
+                dish.comments.id(req.params.commentId).comment = req.body.comment;
+            }
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                // res.json() will take json object as parameter and sents backs to the client
+                res.json(dish);
+            })
+        } else if (dish == null) {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }    
+    }, err => next(err))
+    .catch((err) => next(err))})
+// delete the specified comment for the specified dish if exists
+.delete((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    // Here dish is removed dish
+    .then((dish) => {
+        if (dish !== null && dish.comments.id(req.params.commentId) !== null) {
+            dish.comments.id(req.params.commentId).remove();
+            dish.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                // res.json() will take json object as parameter and sents backs to the client
+                res.json(dish);
+            })
+        } else if (dish == null) {
+            err = new Error('Dish ' + req.params.dishId + ' not found');
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err);            
+        }    
     }, err => next(err))
     .catch((err) => next(err))
 })
